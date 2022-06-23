4bd226b98690e7a7308bde541ddaa5a861f8caf9
thaisdavilla <thaisdavilla@yahoo.com.br>
2022-06-23 08:55:47 -0300

    Exerciocios de arrays do curso Vai na Web.

---
+++ script.js
@@ -0,0 +1,29 @@
+// Crie um array que receba 5 itens e exiba no console.
+let alunos = ["Aline","Bianca","Catarina","Daniel","Eduardo"]
+console.log(alunos)
+
+// Utilize um método para adicionar um nome ao inicio do array.
+let compras = ["Café", "Arroz", "Feijão", "Macarrão", "Sorvete"]
+compras.unshift ("Ovos")
+console.log(compras)
+
+// Utilize um método para remover o último nome do array.
+let pizzas = ["Muçarela", "Portuguesa", "Calabresa", "Marguerita", "Romeu e Julieta"]
+pizzas.pop()
+console.log(pizzas)
+
+// Utilize um método para adicionar dois nomes ao fim do array.
+let discentes = ["Aline","Bianca","Catarina","Daniel","Eduardo"]
+alunos.push("Felipe", "Gabriel")
+console.log(alunos)
+
+// Utilize um método para remover o primeiro nome do array.
+let data = [05,10,15,20,21,25,30]
+data.shift()
+console.log(data)
+
+// Utilize um método para organizar em ordem crescente o seguinte array:
+ let numbers = [7,5,6,3,8,9,2,1,4]
+ numbers.sort()
+ console.log(numbers)
+
